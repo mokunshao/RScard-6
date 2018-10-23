e337bb8f0fa445555fcc98fa857b92395cdce019
mokunshao <1627510957@qq.com>
2018-10-24 01:28:28 +0800

    优化代码

--- js/sticky-navbar.js
+++ js/sticky-navbar.js
@@ -1,9 +1,30 @@
 !function(){
-    window.addEventListener('scroll',function(){
-        if(window.scrollY>0){
-            topNavBar.classList.add("sticky");
-        }else{
-            topNavBar.classList.remove("sticky");
+    
+    let view = document.querySelector('div#navBar')
+
+    let controller = {
+        view: null,
+        init: function(){
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function(){
+            window.addEventListener('scroll',()=>{
+                if(window.scrollY>0){
+                    this.active()
+                }else{
+                    this.deactive()
+                }
+            })
+        },
+        active: function(){
+            this.view.classList.add("sticky")
+        },
+        deactive: function(){
+            this.view.classList.remove("sticky")
         }
-    })
+    }
+
+    controller.init.call(controller,view)
+
 }.call()
